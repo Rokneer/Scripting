2ed7cae7e21cf9d2dadf1013e10751654db6dd03
Alejandro Hincapie <58157472+Rokneer@users.noreply.github.com>
2020-01-30 20:06:06 -0500

    Add files via upload

---
+++ Tarea 3-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese su salario: ");
+            double sal = double.Parse(Console.ReadLine());
+            double salMin = (sal / 877803.0);
+
+            if (salMin < 2)
+            {
+                Console.WriteLine("Aplica a tarifa A");
+            }
+            else if (2 <= salMin && salMin < 4)
+            {
+                Console.WriteLine("Aplica a tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Aplica a tarifa C");
+            }
+        }
+    }
+    
+}
---
+++ Tarea 3-2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese su salario: ");
+            double sal = double.Parse(Console.ReadLine());
+            double salMin = (sal / 877803.0);
+
+            if (salMin < 2)
+            {
+                Console.WriteLine("Aplica a tarifa A y por consulta cancela $3.400");
+            }
+            else if (2 <= salMin && salMin < 4)
+            {
+                Console.WriteLine("Aplica a tarifa B por consulta cancela $13.500");
+            }
+            else
+            {
+                Console.WriteLine("Aplica a tarifa C por consulta cancela $35.600");
+            }
+        }
+    }
+}
